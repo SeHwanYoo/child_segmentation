273f6acecc6aa2621f292a658e742038a53dc7a2
Sehwan Yoo <krstyle03v@gmail.com>
2023-05-10 12:43:28 +0800

    .

--- train.py
+++ train.py
@@ -120,10 +120,10 @@ def main():
         transforms.ToTensor(),
     ])
     
-    Train_Dataset = datasets.SignalDataset(path, transform=transform, is_test=False)
+    Train_Dataset = datasets.SegDataset(path, transform=transform, is_test=False)
     train_dataset = DataLoader(Train_Dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, )
     
-    Test_Dataset = datasets.SignalDataset(path, is_test=True)
+    Test_Dataset = datasets.SegDataset(path, is_test=True)
     test_dataset = DataLoader(Test_Dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, )
   
     loss_function = models.bayesian_loss()
