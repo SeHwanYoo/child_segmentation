234d5c68ed2a51cce52c14a0da8c23ea4d11c1ca
Sehwan Yoo <krstyle03v@gmail.com>
2023-05-10 12:41:05 +0800

    .

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-path = '../../datasets/vitalDB'
+path = '../../datasets/Atopy Segmentation'
 
 
 def parse_args():
